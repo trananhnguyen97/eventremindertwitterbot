fa6d9d8d5027560d82ab947fa06a06306f50b918
Cameron346 <ogdencam@gmail.com>
2018-09-09 04:37:59 -0600

    Processing DMs

--- submit.py
+++ submit.py
@@ -3,4 +3,12 @@ import tweepy
 import auth
 
 api1 = tweepy.API(auth.auth)
-print(api1.direct_messages(count=1)[0].text)
+print(api1.direct_messages(count=100)[0].text)
+print(api1.direct_messages(count=100, since_id=0)[1].text)
+numDM = len(api1.direct_messages())
+print("Number of DMs: ", numDM)
+
+for i in range(0, numDM):
+    dm=api1.direct_messages(count=200)[i].text
+    if not (dm.lower().find("event")==-1):
+        print(dm)
