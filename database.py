162eb4aee6700dd133409a80100080c91f449f64
ThisIsMyUse <u0948300@umail.utah.edu>
2018-09-09 11:28:08 -0600

    File in and out work as they are supposed to now.

--- database.py
+++ database.py
@@ -24,13 +24,13 @@ def search_for_duplicate(name):
 #             listofevents.remove(x)
 
 
-# def name_Lookup(name):
-#     return_list = []
-#     for x in listofevents:
-#         if name == listofevents.index(x).eventName:
-#             return_list.append(listofevents.index(x))
-#     return return_list
-#
+def name_Lookup(name):
+    return_list = []
+    for x in listofevents:
+        if name == listofevents.index(x).eventName:
+            return_list.append(listofevents.index(x))
+    return return_list
+
 #
 # def dateLookup(date):
 #     return_list = []
@@ -59,13 +59,31 @@ def is_there_something_to_post_today():
             event_list_today.append(x)
         if next_week_date == x.eventDate:
             event_list_today.append(x)
-    #return event_list_today, event_list_within_the_week
 
 
-# list_today = []
-# list_next_week = []
-#list_today, list_next_week = is_there_something_to_post_today()
+def read_from_file():
+    f = open("events.txt", 'r')
+
+    # text_line = f.readline()
+    # print(text_line)
+    for line in f:
+        x = line.split(', ', 6)
+        create_event(x[0], x[1], x[2], x[3], x[4], x[5])
+    f.close()
 
 
-list_months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October","November", "December"]
-print(now.month)
+
+def write_to_file():
+   f = open("events.txt", "w")
+   for line in listofevents:
+       f.write(line.eventName + ", " + line.eventDate + ", " + line.eventDesc + ", " + line.eventWeb + ", " + line.eventCategory + ", " + line.eventFood+"\n")
+   f.close()
+
+
+create_event("NAME", "September 9", "DESCRIPTION", "WEBADRESS", "CATEGORY", "FOOD YES")
+create_event("NAME", "September 9", "DESCRIPTION", "WEBADRESS", "CATEGORY", "FOOD YES")
+# write_to_file()
+read_from_file()
+
+if search_for_duplicate("NAME"):
+    print("There was a duplicate")
