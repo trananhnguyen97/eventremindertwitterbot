39a19a87382939652b0a930a7f98322b80af6a6f
Cameron346 <ogdencam@gmail.com>
2018-09-09 02:57:59 -0600

    Created eventClass, main, and submit files

---
+++ eventClass.py
@@ -0,0 +1,6 @@
+class Event:
+    def __init__(self, eventName, eventDate, eventDesc, eventCategory):
+        self.eventName = eventName
+        self.eventDate = eventDate
+        self.eventDest = eventDesc
+        self.eventCategory = eventCategory
