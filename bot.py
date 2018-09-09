5134107670bf32720619eb4158be830881efe664
trananhnguyen97 <trananhnguyen97@gmail.com>
2018-09-09 02:40:44 -0600

    First change

---
+++ bot.py
@@ -0,0 +1,15 @@
+import tweepy
+from auth import *
+
+#create an OAuthHandler instance
+# Twitter requires all requests to use OAuth for authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+auth.set_access_token(access_token, access_secret)
+
+ #Construct the API instance
+api = tweepy.API(auth) # create an API object
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
