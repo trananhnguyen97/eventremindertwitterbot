c7a620cbf0ebfa40ceb8ba9c4cec2f1fa111e884
trananhnguyen97 <trananhnguyen97@gmail.com>
2018-09-09 07:21:01 -0600

    Tweet event category

--- main.py
+++ main.py
@@ -1,23 +1,52 @@
 import tweepy
 import auth
 import re
+import json
 
 #create a class inherithing from the tweepy  StreamListener
 class BotStreamer(tweepy.StreamListener):
 
-    # Called when a new status arrives which is passed down from the on_data method of the StreamListener
     def on_status(self, status):
+
         username = status.user.screen_name
         status_id = status.id
 
-    def on_status(self, status):
-
         # Accept event information with #postuofuevent
         content = status.text
+
+        # Posting events
         if (bool(re.search("#posteventuofu", content))):
-            auth.api.update_status('@' + status.author.screen_name + ' ' + 'Event posting request accepted', status.id)
-        if(bool(re.search("#geteventuofu", content))):
-            auth.api.update_status('@' + status.author.screen_name + ' ' + 'Information about event: ', status.id)
+            auth.api.update_status('@' + username + ' ' + 'Event posting request accepted', status_id)
+
+        # Getting events category
+        if (bool(re.search("#geteventuofu", content))):
+            category = []
+            date = []
+
+            # Academic events
+            if (bool(re.search("#academic", content))):
+                category.append("#academic ")
+
+            if (bool(re.search("#athletic", content))):
+                category.append("#athletic ")
+
+            if (bool(re.search("#hobby", content))):
+                category.append("#hobby ")
+
+            if (bool(re.search("#other", content))):
+                category.append("#other ")
+
+            # Getting events date
+            if (bool(re.search("#today", content))):
+                date.append("#today ")
+
+            if (bool(re.search("#tomorrow", content))):
+                date.append("#tomorrow ")
+
+            category_str = ''.join(category)
+            date_str = ''.join(date)
+
+            auth.api.update_status('@' + username + ' ' + category_str + 'event ' + date_str + '- ', status_id)
 
 
 myStreamListener = BotStreamer()
